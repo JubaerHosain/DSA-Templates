212c933cbca0b3ac53858685e9dd05cd833d1c11
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-01-31 14:55:46 +0600

    Create Code snippet.cpp

---
+++ C++/Code snippet.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<map>
+using namespace std;
+#define scanArray(arr, n) for(int i = 0; i < n; i++) cin >> arr[i];
+#define printArray(arr, n) for(int i = 0; i < n; i++) cout << arr[i] << " ";
+#define sortA(arr, n) sort(arr, arr+n)
+#define sortAr(arr, n) sort(arr, arr+n, greater<int>())
+#define sortV(v) sort(v.begin(), v.end())
+#define sortVr(v) sort(v.rbegin(), v.rend())
+#define rep(i, a, n) for(int i = a; i < n; i++)
+#define add push_back
+#define long long long
+#define uLong unsigned long
+#define inf 1e9
+int mod = 1e9+7;
+
+/*************User defined function*************/
+void solve() {
+    int test;
+    cin >> test;
+    while(test-- > 0) {
+       
+    }
+}
+
+/*************main function*************/
+int main() {
+    solve();
+    return 0;
+}
