7b695ad06a7bebda237f137a018a3d9ac9ab4701
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-02-02 14:47:54 +0600

    Update Code snippet.cpp

--- C++/Code snippet.cpp
+++ C++/Code snippet.cpp
@@ -1,23 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define all(v) v.begin(), v.end()
-#define allr(v) v.rbegin(), v.rend()
-#define des() greater<>()
-#define rep(i, a, n) for(int i = a; i < n; i++)
-#define rrep(i, n, a) for(int i = n; i >= a; i--)
-#define umap unordered_map
-#define uset unordered_set
-#define add push_back
-#define ll long long
-#define ull unsigned long long
-#define mod(a, b, p) ((a%p)*(b%p))%p
-#define newline cout << endl;
-#define scanArray(arr, n) for(int i = 0; i < n; i++) cin >> arr[i];
-#define printArray(arr, n)                  \
-    for(int i = 0; i < n; i++) {            \
-        if(i==n-1) cout << arr[i] << endl;  \
-        else cout << arr[i] << " ";         \
-    }
+#define all(v)          v.begin(), v.end()
+#define allr(v)         v.rbegin(), v.rend()
+#define des()           greater<>()
+#define rep(i, a, n)    for(int i = a; i < n; i++)
+#define rrep(i, n, a)   for(int i = n; i >= a; i--)
+#define umap            unordered_map
+#define uset            unordered_set
+#define add             push_back
+#define ll              long long
+#define ull             unsigned long long
+#define mod(a, b, p)    ((a%p)*(b%p))%p
+#define newline         cout << endl;
+#define scana(arr, n)   for(int i = 0; i < n; i++) cin >> arr[i];
+#define printa(arr)     for(int val: arr) cout << val << " ";
+#define prints(var)     cout << var << endl;
 //#define inf 1e9
 //#define mod 1e9+7
 
