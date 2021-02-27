2a996d9750bf8db26aedcff3bffd8b767dc72d4b
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-02-28 01:14:25 +0600

    Create Dijkstra: Shortest Reach.cpp

---
+++ C++/Dijstra Algorithm/Dijkstra: Shortest Reach.cpp
@@ -0,0 +1,26 @@
+/*************Dijkstra Algo*************/
+vector<pll> g[N];                               //{v, w}
+ll dist[N];
+bool used[N];
+
+void dijkstra(int u) {
+    pqueue<pll, vector<pll>, greater<pll>> pq;  //{w, v}
+    pq.push({0, u});
+    dist[u] = 0;
+
+    while(!pq.empty()) {
+        u = pq.top().second;
+        pq.pop();
+
+        if(used[u]) continue;
+        used[u] = true;
+
+        for(auto a: g[u]) {
+            int v = a.first, w = a.second;
+            if(dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}
