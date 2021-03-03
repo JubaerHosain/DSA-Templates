62ac0122b8d9c3a06beccef9eff6c9dff936bae0
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-03 19:11:30 +0600

    Create Is valid expression.cpp

---
+++ C++/String/Is valid expression.cpp
@@ -0,0 +1,11 @@
+bool is_valid(string a) {
+    stack<char> s;
+ 
+    for(char c: a) {
+        if(c == '(') s.push(')');
+        else if(!s.empty() && s.top() == c) s.pop();
+        else return false;
+    }
+ 
+    return s.empty();
+}
