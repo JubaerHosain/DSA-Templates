1bd4238b4c768fd8c0ce1a352a1296d68314c160
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-05-11 21:33:19 +0600

    Create range of mamimum sum.cpp

---
+++ C++/Kadane’s Algo/range of mamimum sum.cpp
@@ -0,0 +1,15 @@
+vector<int> v;
+
+pii kadens_algo(int n) {
+    int mx = 0, curr = 0;
+    int l = 0, r = 0;
+
+    int pos = 0;
+    rep(i, 0, n) {
+        if(v[i]+curr > v[i]) curr += v[i];
+        else curr = v[i], pos = i;
+        if(curr > mx) mx = curr, l = pos, r = i; 
+    }
+
+    return {l, r};
+}
