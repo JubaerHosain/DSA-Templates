65f2c5ac5682047659320cd0868ed50aefc2ec9b
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-06-23 09:08:58 +0600

    Create starting and ending time.cpp

---
+++ C++/Tree/starting and ending time.cpp
@@ -0,0 +1,3 @@
+Initially, T equals zero. Now we have a beautiful structure giving us so much information about the tree. 
+Consider all segments [tinv;toutv]. We can see that is no pair of intersecting segments. 
+The pair of segments [tinv;toutv] and [tinu;toutu] is either non-intersecting at all or one segment lies inside the other one.
