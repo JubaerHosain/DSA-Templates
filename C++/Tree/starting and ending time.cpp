a0520fb83680031c108f6bf61c37c8e73a8ddefc
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-06-23 09:15:01 +0600

    Update starting and ending time.cpp

--- C++/Tree/starting and ending time.cpp
+++ C++/Tree/starting and ending time.cpp
@@ -1,3 +1,4 @@
 Initially, T equals zero. Now we have a beautiful structure giving us so much information about the tree. 
 Consider all segments [tinv;toutv]. We can see that is no pair of intersecting segments. 
 The pair of segments [tinv;toutv] and [tinu;toutu] is either non-intersecting at all or one segment lies inside the other one.
+The second beautiful fact is that for each vertex u in the subtree of v the segment [tinu;toutu] lies inside the segment [tinv;toutv].
