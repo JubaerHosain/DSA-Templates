177b09c303084521d81d5986f86f3cf272c77524
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-02 00:06:59 +0600

    Create Longest Increasing Subsequence(nlogn).java

---
+++ C++/Dynamic Programming/Longest Increasing Subsequence(nlogn).java
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl          '\n'//notforinter
+#define all(v)        v.begin(), v.end()
+#define print(var)    cout << var << endl
+#define out(a, b)     cout << a << " " << b << endl
+#define rep(i, s, n)  for(int i = s; i < n; i++)
+#define rrep(i, n, s) for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+
+/*****User defined function*****/
+int mod = 1e9+7;
+int inf = 1e9+5;
+
+int get_best(map<int, int> &mp, int ai) {
+    auto it = mp.lower_bound(ai);
+    if(it == mp.begin())
+        return 0;
+    return (--it)->second;
+}
+
+void insert(map<int, int> &mp, int ai, int cnt) {
+    if(mp[ai] >= cnt)
+        return;
+
+    mp[ai] = cnt;
+    auto it = ++mp.find(ai);
+    while(it != mp.end() && it->second <= cnt) {
+        auto tmp = it++;
+        mp.erase(tmp);
+    }
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    rep(i, 0, n) cin >> a[i];
+
+    map<int, int> mp;
+    vector<int> dp(n);
+    dp[0] = mp[a[0]] = 1;
+    rep(i, 1, n) {
+        dp[i] = 1 + get_best(mp, a[i]);
+        insert(mp, a[i], dp[i]);
+    }
+
+    print(*max_element(all(dp)));
+}
+
+/*****main function*****/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(16);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
