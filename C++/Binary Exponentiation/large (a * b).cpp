eddd5556a3fed0733ef985531bbfdf78afa36ad2
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-03 19:50:00 +0600

    Update large (a * b).cpp

--- C++/Binary Exponentiation/large (a * b).cpp
+++ C++/Binary Exponentiation/large (a * b).cpp
@@ -4,3 +4,27 @@
   a * b = a + a + … + a (b times) 
 */
 
+ll mod_mul(ll a, ll b, ll p) {
+    ll res = 0;
+    while(b) {
+        if(b&1)
+            res = (res + a) % p;
+        b >>= 1;
+        a = (a + a) % p;
+    }
+
+    return res;
+}
+
+ll mod_pow(ll a, ll n, ll p) {
+    ll res = 1;
+
+    while(n) {
+        if(n&1) 
+            res = mod_mul(res, a, p);
+        n >>= 1;
+        a = mod_mul(a, a, p);
+    }
+
+    return res;
+}
