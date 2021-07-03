1d50f55c26cba1f98bbebc1ea889166f6c25ae7f
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-03 17:03:16 +0600

    Create large (a * b).cpp

---
+++ C++/Binary Exponentiation/large (a * b).cpp
@@ -0,0 +1,6 @@
+/*
+  Efficient solution : Since a and b may be very large numbers, if we try to multiply directly then it will definitely overflow. 
+  Therefore we use the basic approach of multiplication i.e.,
+  a * b = a + a + … + a (b times) 
+*/
+
