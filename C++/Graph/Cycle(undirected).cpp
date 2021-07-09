8bd7ed2d224a511ca0022e89540164290287e025
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-09 11:11:28 +0600

    Create Cycle(undirected).cpp

---
+++ C++/Graph/Cycle(undirected).cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl          '\n'//notforinter
+#define all(v)        v.begin(), v.end()
+#define print(var)    cout << var << endl
+#define rep(i, s, n)  for(int i = s; i <= n; i++)
+#define rrep(i, n, s) for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+ 
+/*****User defined function*****/
+const int N = 1e3+5;
+int inf = 1e9, mod = 1e9+7;
+bool used[N];
+vector<int> g[N];
+
+bool cycle(int u, int p) {
+    used[u] = true;
+
+    for(int v: g[u]) {
+        if(!used[v]) {
+            if(cycle(v, u))
+                return true;
+        } else if(v != p) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    rep(i, 1, m) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    memset(used, false, sizeof(used));
+    rep(u, 1, n) {
+        if(!used[u] && cycle(u, -1)) {
+            print("YES");
+            return;
+        }
+    }
+    
+    print("NO");
+}   
+ 
+/*****main function*****/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+ 
+    cout << fixed << showpoint;
+    cout << setprecision(16);
+ 
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+ 
+    return 0;
+}   
