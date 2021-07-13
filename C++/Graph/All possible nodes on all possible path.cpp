51b685fe39a5229abf47dd0bcfaf18bbf3556a6b
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-13 22:05:40 +0600

    Update All possible nodes on all possible path.cpp

--- C++/Graph/All possible nodes on all possible path.cpp
+++ C++/Graph/All possible nodes on all possible path.cpp
@@ -2,4 +2,5 @@
 //Run Forward DFS and Reverse DFS
 //intersection's of both are the possible vertex path from src to dst.
 //if(vis1[u] && vis2[v])
+//equivalent to if(vis1[u] && vis2[u] && vis1[v] && vis2[v])
  
