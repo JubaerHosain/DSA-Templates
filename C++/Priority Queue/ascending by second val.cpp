944ea75caa5cd353ddaa5d7d317a5c724249f31c
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-18 23:10:00 +0600

    Delete ascending by second val.cpp

--- C++/Priority Queue/ascending by second val.cpp
+++
@@ -1,8 +0,0 @@
-struct cmp { 
-    constexpr bool operator()(pii const& a, pii const& b) 
-    const noexcept { 
-        return a.second > b.second; 
-    } 
-};
-
-//pqueue<pii, vector<pii>, cmp> q;
