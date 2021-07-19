88c75b6bb68cba5433dfd1715c7ae4cc6de83adc
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-19 14:53:15 +0600

    Delete On pair of vector(by first val).cpp

--- C++/Binary Search/On pair of vector(by first val).cpp
+++
@@ -1,8 +0,0 @@
-struct compare { 
-    bool operator()(const pii& value, const int& key) { 
-        return (value.first < key); 
-    } 
-    bool operator()(const int& key, const pii& value) { 
-        return (key < value.first); 
-    } 
-}; 
