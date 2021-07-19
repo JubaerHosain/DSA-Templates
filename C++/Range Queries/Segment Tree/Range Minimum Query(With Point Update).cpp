1079e767ff7cea9137ea11280ee452912afd4c99
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-19 15:30:16 +0600

    Update Range Minimum Query(With Point Update).cpp

--- C++/Range Queries/Segment Tree/Range Minimum Query(With Point Update).cpp
+++ C++/Range Queries/Segment Tree/Range Minimum Query(With Point Update).cpp
@@ -11,12 +11,11 @@ typedef pair<int, int> pii;
 typedef vector<vector<int>> vii;
  
 /*****User defined function*****/
-ll inf = 1e18;
 const int N = 2e5+5;
 vector<int> a(N);
-vector<ll> tree(4*N);
+vector<int> tree(4*N);
 
-ll buildST(int i, int l, int r) {
+int buildST(int i, int l, int r) {
     if(l == r) 
         return tree[i] = a[l];
     
@@ -24,11 +23,11 @@ ll buildST(int i, int l, int r) {
     return tree[i] = min(buildST(2*i+1, l, mid), buildST(2*i+2, mid+1, r));
 }
 
-ll get_min(int i, int sl, int sr, int l, int r) {
+int get_min(int i, int sl, int sr, int l, int r) {
     if(sl >= l && sr <= r)          // total overlap
         return tree[i];
     else if(sr < l || r < sl)       // no overlap;
-        return inf;
+        return INT_MAX;
 
     int mid = (sl + sr) / 2;
     return min(get_min(2*i+1, sl, mid, l, r), get_min(2*i+2, mid+1, sr, l, r));
@@ -49,7 +48,7 @@ void update(int i, int sl, int sr, int pos, int val) {
     tree[i] = min(tree[2*i+1], tree[2*i+2]);               //update intermediate nodes after returning the value of the leaf node
 }
 
-ll get_min(int l, int r, int n) {
+int get_min(int l, int r, int n) {
     return get_min(0, 0, n-1, l, r);
 }
 
