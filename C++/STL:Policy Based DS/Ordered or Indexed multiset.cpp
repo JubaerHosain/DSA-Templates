b535a785931fac9457f4f01e9e59b68f1c67dca5
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-21 22:09:25 +0600

    Update Ordered or Indexed multiset.cpp

--- C++/STL:Policy Based DS/Ordered or Indexed multiset.cpp
+++ C++/STL:Policy Based DS/Ordered or Indexed multiset.cpp
@@ -1,4 +1,4 @@
-//best use
+//best use============
 template<class T> using ordered_set = tree<T, null_type, 
 less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 //it would be declared as ordered_set<type> variable_name;
@@ -11,11 +11,15 @@ using namespace __gnu_pbds;
 //rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 
-
+================
 //for set use less<type> comparator
 //for multiset use less_equal<type> instead of less
 typedef tree<int, null_type, less<int>,
 rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+=================
+or can try this
+There is no implemented tree multiset in STL. So, 
+I have used pair<T,int> as a key where the second element in pair is the time when item has been added, in order to maintain uniqueness of elements.
 
 //less_equal for ordered_multiset
  
