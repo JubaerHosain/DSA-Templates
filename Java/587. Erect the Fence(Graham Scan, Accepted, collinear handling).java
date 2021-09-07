d29135c1a494e94991dd4b239805bc98083faaa7
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-09-07 10:48:55 +0600

    Update and rename 587. Erect the Fence(Graham Scan, with error).java to 587. Erect the Fence(Graham Scan, Accepted, collinear handling).java

--- Java/587. Erect the Fence(Graham Scan, Accepted, collinear handling).java
+++ Java/587. Erect the Fence(Graham Scan, Accepted, collinear handling).java
@@ -1,4 +1,76 @@
 class Solution {
+    int orientation(int[] a, int[] b, int[] c) {
+        //val = 0 collinear, val > 0 clockwise, val < 0 anti-clockwise
+        return (b[1] - a[1])*(c[0] - b[0]) - (c[1] - b[1])*(b[0] - a[0]);
+    }
+
+    int distance(int[] a, int[] b) {
+        return (a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]);
+    }
+    
+    int[] findBottomLeft(int[][] points, int n) {
+        int[] bottomLeft = points[0];
+        
+        for(int i = 1; i < n; i++) {
+            if(points[i][1] < bottomLeft[1])
+                bottomLeft = points[i];
+            else if(points[i][1] == bottomLeft[1])
+                bottomLeft = (points[i][0] < bottomLeft[0]) ? points[i] : bottomLeft;
+        }
+        
+        return bottomLeft;
+    }
+    
+    public int[][] outerTrees(int[][] points) {
+        if(points.length <= 3)
+            return points;
+        
+        int n = points.length;
+        int[] start = findBottomLeft(points, n);
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                if(orientation(start, a, b) < 0) 
+                    return -1;
+                else if(orientation(start, a, b) > 0)
+                    return 1;
+                else
+                    return distance(start, a) - distance(start, b);
+            } 
+        });
+        
+        //if collinear point in the end then reverse those
+        //points for min distance point stays at far distance from start
+        int i = n-1;
+        while(i >= 0 && orientation(start, points[i], points[n-1]) == 0)
+            i--;
+        
+        int left = i+1, right = n-1;
+        while(left < right) {
+            int[] tmp = points[left];
+            points[left] = points[right];
+            points[right] = tmp;
+            left++; right--;
+        }
+        
+        Stack<int[]> stack = new Stack<>();
+        stack.push(points[0]);
+        stack.push(points[1]);
+        for (int j = 2; j < points.length; j++) {
+            int[] top = stack.pop();
+            while (orientation(stack.peek(), top, points[j]) > 0)
+                top = stack.pop();
+            stack.push(top);
+            stack.push(points[j]);
+        }
+        
+        return stack.toArray(new int[stack.size()][]);
+    }
+}
+
+
+//=========================================================================
+//this code gives wrong answer for not handling collinear points at the end
+class Solution {
     class Point {
         int x, y;
         
