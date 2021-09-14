07beca877efb45e8746986d2e53a29d7f5ee42e9
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-09-14 19:37:16 +0600

    Create Maximum XOR using K numbers from 1 to N.cpp

---
+++ C++/Bitwise Operation/Maximum XOR using K numbers from 1 to N.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxXorSum(int n, int k) {
+	if (k == 1)
+		return n;
+
+	int res = 1;
+	while (res <= n)
+		res <<= 1;
+
+	//return 2^exp - 1
+    //exp = 32 - __builtin_clz(n);
+
+	return res - 1;
+}
+
+// Driver program
+int main() {
+	int n = 12, k = 7;
+	cout << maxXorSum(n, k) << endl;
+	return 0;
+}
