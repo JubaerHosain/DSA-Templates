27cd4b6703a718c9557715c605b6d551bb0cb8e5
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-11-17 19:26:41 +0600

    Create Sudoku Solver.cpp

---
+++ C++/Backtracking/Sudoku Solver.cpp
@@ -0,0 +1,76 @@
+// TLE............................................
+
+class Solution {    
+    bool is_valid(vector<vector<char>>& board, int x, int y, char val) {
+        int n = board.size();
+        
+        // check row 
+        for(int j = 0; j < n; j++) {
+            if(board[x][j] == val)
+                return false;
+        }
+        
+        // check col 
+        for(int i = 0; i < n; i++) {
+            if(board[i][y] == val)
+                return false;
+        }
+        
+        // find top left corner of that subgrid 
+        int tx = 3 * (x/3);
+        int ty = 3 * (y/3);
+        
+        // check subgrid
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(board[tx+i][ty+j] == val)
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    bool solveSudoku(vector<vector<char>>& board, int x, int y) {
+        if(x == board.size()) {
+            return true;
+        }
+        
+        // calculate next position 
+        int nx, ny;
+        if(y == board.size()-1) {
+            nx = x + 1;
+            ny = 0;
+        } 
+        else {
+            nx = x;
+            ny = y + 1;
+        }
+        
+        if(board[x][y] != '.') {
+            solveSudoku(board, nx, ny);
+        } 
+        else {
+            for(int val = 1; val <= 9; val++) {
+                if(is_valid(board, x, y, (char)val)) {
+                    board[x][y] = (char)val;
+                    if(solveSudoku(board, nx, ny))
+                        return true;
+                    // this is backtracking 
+                    // restore the value if condition is not fulfilled
+                    board[x][y] = '.';
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solveSudoku(board, 0, 0);
+        
+        //board changed internally
+    }
+};
