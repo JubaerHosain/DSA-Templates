4325c30de8fc7e4c5a0f68e6d34eeecaccb1bb22
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-11-24 02:07:00 +0600

    Update Longest Increasing Subsequence(n^2).java

--- C++/Dynamic Programming/Longest Increasing Subsequence(n^2).java
+++ C++/Dynamic Programming/Longest Increasing Subsequence(n^2).java
@@ -6,6 +6,7 @@ class Solution {
         int count = 1;
         for(int i = 1; i < nums.length; i++) {
             for(int j = 0; j < i; j++) {
+                // <= for longest non-decreasing subseq, non-decreasing means next element is greater or equal. 2 2 3 is valid.
                 if(nums[j] < nums[i]) {
                     dp[i] = Math.max(dp[i], 1+dp[j]);
                     count = Math.max(count, dp[i]);
