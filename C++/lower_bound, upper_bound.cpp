71d8dac6851c790a527264c7163b281ffefc1d9e
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-12-07 02:32:25 +0600

    Delete lower_bound, upper_bound.cpp

--- C++/lower_bound, upper_bound.cpp
+++
@@ -1,2 +0,0 @@
-lower_bound returns an iterator pointing to the first element in the range [first,last) which has a value "not less than ‘val’". 
-upper_bound returns an iterator pointing to the first element in the range [first,last) which has a value "greater than ‘val’". 
