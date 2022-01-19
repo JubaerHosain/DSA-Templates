3a6e8c17bd594bd484210a64caa5b7b279ee635f
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2022-01-19 17:29:39 +0600

    Create Template.cpp

---
+++ C++/Template.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl    '\n'
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+
+/*****User defined function*****/
+void solve_problem() {
+    cout << "Hello World" << endl;
+} 
+ 
+/*****main function*****/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+ 
+    cout << fixed << showpoint;
+    cout << setprecision(5);
+ 
+    int test = 1;
+    //cin >> test;
+    while(test--)  solve_problem();
+ 
+    return 0;
+} 
