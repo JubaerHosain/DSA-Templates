6f223528e6a85ba349bf5cb56c1bc24887b7eaca
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2022-03-02 13:59:21 +0600

    Create Integer Factors.cpp

---
+++ C++/Math/Integer Factors.cpp
@@ -0,0 +1,8 @@
+ vector<int> fact;
+    for(int i = 1; i*i <= n; i++) {
+        if(n%i == 0) {
+            fact.push_back(i);
+            if(i != n/i)
+                fact.push_back(n/i);
+        }
+    }
