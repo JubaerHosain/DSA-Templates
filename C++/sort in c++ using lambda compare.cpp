484236debb9a4a47ec9e8b7d534019bac51d7f54
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2022-03-25 23:32:34 +0600

    Update sort in c++ using lambda compare.cpp

--- C++/sort in c++ using lambda compare.cpp
+++ C++/sort in c++ using lambda compare.cpp
@@ -1,5 +1,6 @@
 sort(costs.begin(), costs.end(), [](vector<int> a, vector<int> b) {
             return (a[1] - a[0]) < (b[1] - b[0]);
-            // giving "<" sign rather than "-" is better
+            // giving "<" sign rather than "-" 
             // see leetcode 1029. Two City Scheduling
+            // - is not working 
         });
